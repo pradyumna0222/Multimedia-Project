2a398d388f6c10f4f79b2514ddf36c994f6cca84
pradyumna0222 <ramadurgampradyumna@gmail.com>
2025-12-02 20:13:16 +0530

    image compression process using K-Means clustering.

--- question2.py
+++ question2.py
@@ -1,10 +1,14 @@
+# This code performs image compression using K-Means clustering.
+# It reads an image, reduces its number of colors to 16 by clustering similar pixels.
 import cv2, numpy as np
 from sklearn.cluster import KMeans
-
 img = cv2.imread("boat.png"); h,w = img.shape[:2]; X = img.reshape(-1,3)
+# Apply K-Means clustering with 16 color clusters
 k = KMeans(n_clusters=16).fit(X)
+# Replace each pixel with the color of its assigned cluster center
 out = k.cluster_centers_[k.labels_].astype("uint8").reshape(h,w,3)
 cv2.imwrite("compressed.png", out)
+
 
 
 
