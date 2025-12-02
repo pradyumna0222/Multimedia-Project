3e3772a095b681a6228300a71dc1ec93c90fba1c
pradyumna0222 <ramadurgampradyumna@gmail.com>
2025-12-02 19:58:23 +0530

    Implement image quantization methods
    Added code for gray, median-cut, and octree quantization of an image.

--- question1.py
+++ question1.py
@@ -1,9 +1,13 @@
+# Added gray, median-cut and octree quantization code
 from PIL import Image
 import numpy as np
+# grey quantization
 img = np.array(Image.open('tiger.png').convert('RGB'))
 gray = ((img[:,:,0] + img[:,:,1] + img[:,:,2]) // 3).astype('uint8')
 Image.fromarray(gray).save('grey.png')
+# median-cut quantization
 img = Image.open('tiger.png')
 q = img.quantize(colors=16, method=Image.MEDIANCUT)
 q.save('mediancut.png')
+# octree quantization
 Image.open('tiger.png').quantize(colors=16, method=Image.FASTOCTREE).save('octree.png')
